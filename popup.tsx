1b5815e26f2a899ef705ec8360c2d04d61ed673b
Alex Shevlyakov <pxl.alexjedi@gmail.com>
2024-08-18 10:42:53 +0200

    Add custom shadcn

--- popup.tsx
+++ popup.tsx
@@ -1,5 +1,6 @@
-import * as Switch from "@radix-ui/react-switch"
 import { useEffect, useState } from "react"
+
+import { Switch } from "~/components/Switch"
 
 import "./styles.css"
 
@@ -53,20 +54,17 @@ function IndexPopup() {
   }
 
   return (
-    <section className="bg-white w-[200px] h-[180px] text-gray-800 p-4 space-y-4 flex justify-between items-end font-['Inter',sans-serif]">
+    <section className="dark bg-background text-primary w-[200px] h-[180px] p-4 space-y-4 flex justify-between items-end font-['Inter',sans-serif]">
       <div className="flex flex-col items-left -mb-1">
         <h2 className="text-lg font-medium">Make</h2>
         <h2 className="text-lg font-medium">Figma</h2>
         <h2 className="text-lg font-medium">Great</h2>
         <h2 className="text-lg font-medium">Again</h2>
       </div>
-      <Switch.Root
+      <Switch
         checked={isEnabled}
         onCheckedChange={handleToggleStyles}
-        className="peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-black data-[state=unchecked]:bg-gray-300"
-        id="make-figma-great-again">
-        <Switch.Thumb className="pointer-events-none block h-4 w-4 rounded-full bg-white shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0" />
-      </Switch.Root>
+        id="make-figma-great-again"></Switch>
       {/* {message && <p>{message}</p>} */}
     </section>
   )
